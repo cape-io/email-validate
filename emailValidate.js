7e3f239776ca28c39de4fa397f34929b060e2d95
Kai Curry <kai@sundaysenergy.com>
2015-09-23 19:01:02 -0500

    babel

---
+++ emailValidate.js
@@ -0,0 +1,108 @@
+import {validate} from 'email-validator';
+import fetchJsonp from 'fetch-jsonp';
+import startsWith from 'lodash.startswith';
+import includes from 'lodash.includes';
+
+const emailIndex = {};
+
+export default function(apiKey, address, cb) {
+  // cache
+  if (emailIndex[address]) {
+    return {...emailIndex[address], breakPoint: 'cache'}
+  }
+
+  const status = function(help, breakPoint = 'help') {
+    const v = {
+      value: address,
+      breakPoint: breakPoint,
+      hasErrors: true,
+      suggestion: null,
+      help
+    };
+    if (address) {
+      emailIndex[address] = v;
+    }
+    return v;
+  }
+
+  // help.
+  if (!address) {
+    return status(null);
+  }
+  if (!includes(address, '@')) {
+    return status('The email must contain an "@" symbol.');
+  }
+  const parts = address.split('@');
+  const local = parts[0];
+  if (!local) {
+    return status('Please include the unique username part before the "@" symbol.');
+  }
+  const domain = parts[1];
+  if (!domain) {
+    return status('Please include a valid domain for your email.');
+  }
+  if (!includes(domain, '.')) {
+    return status('The domain part of your email needs a tld.');
+  }
+  const tld = domain.split('.')[1];
+  if (!(tld.length > 1)) {
+    return status('The email domain tld is too short.');
+  }
+
+  // preCheck
+  if (!validate(address)){
+    return status('Failed RFC checks.', 'preCheck');
+  }
+
+  // console.log('checkAddress', 'mailgun', apiKey, address);
+  if (!startsWith(apiKey, 'pubkey-')) {
+    throw new Error('apiKey must be a mailgun public key!');
+  }
+
+  const mailgun = 'https://api.mailgun.net/v3/address/validate';
+  const qs = `?api_key=${apiKey}&address=${address}`;
+
+  const triggerCb = function(v){
+    if (address) {
+      emailIndex[address] = v;
+    }
+    cb(v);
+  }
+
+  fetchJsonp(mailgun+qs, {timeout: 1351})
+  .then(function(response) {
+    return response.json();
+  })
+  .then((data) => {
+    const suggestion = data.did_you_mean;
+    const hasErrors = !data.is_valid;
+    const help = 'Failed DNS validation or the Email Service Provider could not validate the address.';
+    return triggerCb({
+      value: address,
+      breakPoint: 'async',
+      hasErrors: hasErrors,
+      help: hasErrors ? help : 'This email address looks valid.',
+      suggestion: suggestion
+    })
+  })
+  .catch(function(ex) {
+    console.log('validateEmail failed', address, ex);
+    if (!navigator.onLine) {
+      alert('Please check your internet connection. It looks like you are disconnected.');
+    }
+    // Try another API? kickbox.io / mailboxlayer.com
+    triggerCb({
+      value: address,
+      breakPoint: 'async-error',
+      hasErrors: false,
+      help: 'Looks valid. We were not able to do DNS or SMTP checking.',
+      suggestion: null
+    })
+  });
+  return {
+    value: address,
+    breakPoint: 'preCheck',
+    hasErrors: false,
+    help: 'Checking DNS and ESP...'
+  };
+}
--- index.js
+++ index.js
@@ -1,91 +1,109 @@
-import {validate} from 'email-validator';
-import fetchJsonp from 'fetch-jsonp';
-import startsWith from 'lodash.startswith';
-import includes from 'lodash.includes';
+'use strict';
 
-const emailIndex = {};
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
 
-export default validateEmail(apiKey, address, cb) {
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var _emailValidator = require('email-validator');
+
+var _fetchJsonp = require('fetch-jsonp');
+
+var _fetchJsonp2 = _interopRequireDefault(_fetchJsonp);
+
+var _lodashStartswith = require('lodash.startswith');
+
+var _lodashStartswith2 = _interopRequireDefault(_lodashStartswith);
+
+var _lodashIncludes = require('lodash.includes');
+
+var _lodashIncludes2 = _interopRequireDefault(_lodashIncludes);
+
+var emailIndex = {};
+
+exports['default'] = function (apiKey, address, cb) {
   // cache
   if (emailIndex[address]) {
-    return {...emailIndex[address], breakPoint: 'cache'}
+    return _extends({}, emailIndex[address], { breakPoint: 'cache' });
   }
 
-  const status = function(help, breakPoint = 'help') {
-    const v = {
+  var status = function status(help) {
+    var breakPoint = arguments.length <= 1 || arguments[1] === undefined ? 'help' : arguments[1];
+
+    var v = {
       value: address,
       breakPoint: breakPoint,
       hasErrors: true,
       suggestion: null,
-      help
+      help: help
     };
     if (address) {
       emailIndex[address] = v;
     }
     return v;
-  }
+  };
 
   // help.
   if (!address) {
     return status(null);
   }
-  if (!includes(address, '@')) {
+  if (!(0, _lodashIncludes2['default'])(address, '@')) {
     return status('The email must contain an "@" symbol.');
   }
-  const parts = address.split('@');
-  const local = parts[0];
+  var parts = address.split('@');
+  var local = parts[0];
   if (!local) {
     return status('Please include the unique username part before the "@" symbol.');
   }
-  const domain = parts[1];
+  var domain = parts[1];
   if (!domain) {
     return status('Please include a valid domain for your email.');
   }
-  if (!includes(domain, '.')) {
+  if (!(0, _lodashIncludes2['default'])(domain, '.')) {
     return status('The domain part of your email needs a tld.');
   }
-  const tld = domain.split('.')[1];
+  var tld = domain.split('.')[1];
   if (!(tld.length > 1)) {
     return status('The email domain tld is too short.');
   }
 
   // preCheck
-  if (!validate(address)){
+  if (!(0, _emailValidator.validate)(address)) {
     return status('Failed RFC checks.', 'preCheck');
   }
 
   // console.log('checkAddress', 'mailgun', apiKey, address);
-  if (!startsWith(apiKey, 'pubkey-')) {
+  if (!(0, _lodashStartswith2['default'])(apiKey, 'pubkey-')) {
     throw new Error('apiKey must be a mailgun public key!');
   }
 
-  const mailgun = 'https://api.mailgun.net/v3/address/validate';
-  const qs = `?api_key=${apiKey}&address=${address}`;
+  var mailgun = 'https://api.mailgun.net/v3/address/validate';
+  var qs = '?api_key=' + apiKey + '&address=' + address;
 
-  const triggerCb = function(v){
+  var triggerCb = function triggerCb(v) {
     if (address) {
       emailIndex[address] = v;
     }
     cb(v);
-  }
+  };
 
-  fetchJsonp(mailgun+qs, {timeout: 1351})
-  .then(function(response) {
+  (0, _fetchJsonp2['default'])(mailgun + qs, { timeout: 1351 }).then(function (response) {
     return response.json();
-  })
-  .then((data) => {
-    const suggestion = data.did_you_mean;
-    const hasErrors = !data.is_valid;
-    const help = 'Failed DNS validation or the Email Service Provider could not validate the address.';
+  }).then(function (data) {
+    var suggestion = data.did_you_mean;
+    var hasErrors = !data.is_valid;
+    var help = 'Failed DNS validation or the Email Service Provider could not validate the address.';
     return triggerCb({
       value: address,
       breakPoint: 'async',
       hasErrors: hasErrors,
       help: hasErrors ? help : 'This email address looks valid.',
       suggestion: suggestion
-    })
-  })
-  .catch(function(ex) {
+    });
+  })['catch'](function (ex) {
     console.log('validateEmail failed', address, ex);
     if (!navigator.onLine) {
       alert('Please check your internet connection. It looks like you are disconnected.');
@@ -97,7 +115,7 @@ export default validateEmail(apiKey, address, cb) {
       hasErrors: false,
       help: 'Looks valid. We were not able to do DNS or SMTP checking.',
       suggestion: null
-    })
+    });
   });
   return {
     value: address,
@@ -105,4 +123,7 @@ export default validateEmail(apiKey, address, cb) {
     hasErrors: false,
     help: 'Checking DNS and ESP...'
   };
-}
+};
+
+module.exports = exports['default'];
+
